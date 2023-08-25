07f21c58a8548838d8b6d46a49ba1088c740b399
santhikc12@gmail.com <shanfshan223@gmail.com>
2023-08-25 16:38:04 +0530

    image url

---
+++ AdminLTE-1/Api/employeeviewinsert.php
@@ -0,0 +1,63 @@
+<?php
+$conn=mysqli_connect("localhost","root","","home_service");
+if(mysqli_connect_error())
+{
+    die("Error in connection");
+}
+
+
+$name =$_POST['name'];
+$email =$_POST['email'];
+$hash=password_hash($_POST['password'],PASSWORD_DEFAULT);
+$address =$_POST['address'];
+$phone_number =$_POST['phone_number'];
+$experience =$_POST['experience'];
+$service_type =$_POST['service_type'];
+$filename=$_FILES["photo"]["name"];
+$tempname=$_FILES["photo"]["tmp_name"];
+$folder = "./image/".$filename;
+$image=$filename;
+
+$uploadOk=1;
+$imageFileType=strtolower(pathinfo($folder,PATHINFO_EXTENSION));
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif")
+{
+    echo "sorry, only JPG,JPEG,PNG & GIF files are allowded.";
+    $uploadOk =0;
+}
+if($uploadOk == 0)
+{
+    echo "sorry";
+}
+else
+{
+    move_uploaded_file($tempname,$folder);
+}
+
+
+
+
+
+  $sql=mysqli_query($conn,"INSERT INTO employee_reg(name,email,password,address,phone_number,experience,service_type,photo) VALUES('$name','$email','$hash','$address','$phone_number','$experience','$service_type','$image')");
+if($sql)
+{
+     $myarray['message'] = 'Added';
+     $query = mysqli_query($conn,"SELECT * FROM employee_reg  ");
+$data=mysqli_fetch_assoc($query);
+if($query)
+{
+     $myarray['data'] =$data ;
+}
+
+}
+else
+{
+    $myarray['message'] = 'failed';
+}
+echo json_encode($myarray);
+
+
+
+
+
+?>
---
+++ AdminLTE-1/Api/veiwapiuser.php
@@ -0,0 +1,58 @@
+<?php
+$conn=mysqli_connect("localhost","root","","home_service");
+if(mysqli_connect_error())
+{
+    die("Error in connection");
+}
+
+
+$user_name =$_POST['user_name'];
+$email =$_POST['email'];
+$hash=password_hash($_POST['password'],PASSWORD_DEFAULT);
+$address =$_POST['address'];
+$phone_number =$_POST['phone_number'];
+$filename=$_FILES["photo"]["name"];
+$tempname=$_FILES["photo"]["tmp_name"];
+$folder = "./image/".$filename;
+$image=$filename;
+
+$uploadOk=1;
+$imageFileType=strtolower(pathinfo($folder,PATHINFO_EXTENSION));
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif")
+{
+    
+    $uploadOk =0;
+    
+}
+if($uploadOk == 0)
+{
+    echo "sorry";
+}
+else
+{
+    move_uploaded_file($tempname,$folder);
+}
+
+
+
+
+
+  $sql=mysqli_query($conn,"INSERT INTO user_reg(user_name,email,password,address,phone_number,photo) VALUES('$user_name','$email','$hash','$address','$phone_number','$image')");
+if($sql)
+{
+     $myarray['message'] = 'Added';
+     $query = mysqli_query($conn,"SELECT * FROM user_reg  ");
+     $data=mysqli_fetch_assoc($query);
+      if($query)
+{
+     $myarray['data'] =$data ;
+}
+}
+else
+{
+    $myarray['message'] = 'failed';
+}
+echo json_encode($myarray);
+
+
+?>
